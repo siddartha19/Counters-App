67d876a6bf6686e86e7ced2ba03711863f6fe873
siddartha19 <gsiddartha01@gmail.com>
2020-07-23 14:04:23 +0530

    Updates

---
+++ precache-manifest.45ef7959ae94603d601f656217dfbc21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35ccda8b96ff94e6482b270a57efefb",
+    "url": "/Counters-App/index.html"
+  },
+  {
+    "revision": "236234f4d27b85e0c5ea",
+    "url": "/Counters-App/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "35d3c4d6592ad4fd2bc9",
+    "url": "/Counters-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "236234f4d27b85e0c5ea",
+    "url": "/Counters-App/static/js/2.5ae3bdd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counters-App/static/js/2.5ae3bdd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d3c4d6592ad4fd2bc9",
+    "url": "/Counters-App/static/js/main.4beb8389.chunk.js"
+  },
+  {
+    "revision": "d6d6b22464318c831dd4",
+    "url": "/Counters-App/static/js/runtime-main.5b346c3f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Counters-App/precache-manifest.45ef7959ae94603d601f656217dfbc21.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Counters-App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
